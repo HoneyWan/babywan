565bb0ce0bddbe9ef58bb2894bea74b37bfddfd0
SugerWan <1438621668@qq.com>
2019-12-02 10:01:29 +0800

    数组的简单举例

--- 7-6.c
+++ 7-6.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
+#define Month 12
 int main(void)
 {
-        
+        printf("打印每个月份的天数:\n");
+        int Years[Month] = {31,28,31,30,31,30,31,31,30,31,30,31};
+        int i;
+        for ( i = 0; i < Month; i ++ )
+        {
+                printf("Month %d has %d day\n",i+1,Years[i]);
+                
+        }
         return 0;
 }
