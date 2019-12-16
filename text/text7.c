b15256a582322b2b6fbe4988c44a1dbb429c1ffb
SugerWan <1438621668@qq.com>
2019-12-16 23:27:29 +0800

    实训4

---
+++ text/text7.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<math.h>
+int main(void)
+{
+        float x,y;
+        printf("请输入一个单精度浮点型:");
+        scanf("%f",&x);
+        if( x > 1 )
+        {
+                if( x >= 10 )
+                {
+                        y = 3 * x - 11;
+                }
+                else
+                {
+                        y = 2 * x - 11;
+                }
+        }
+        else
+        {
+                y = x;
+        }
+        printf("x = %f,y = %f",x,y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /* float x,y; */
+        /* printf("请输入一个单精度浮点型:"); */
+        /* scanf("%f",&x); */
+        /* if( x >= 0 && x <= 1) */
+        /* { */
+        /*         y = exp( sqrt (x) ) - 1; */
+        /* } */
+        /* else if(x >= 3 && x <= 4) */
+        /* { */
+        /*         y = abs(  x ) + 1; */
+        /* } */
+        /* else */
+        /* { */
+        /*         y = sin(x * x); */
+        /* } */
+        /* printf("x = %f, y = %f",x,y); */
+        return 0;
+}
---
+++ text10.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void main()
+{
+
+        int i,n,sum = 0;
+        scanf("%d",&i);
+        n = i;
+        while( i <= 10)
+        {
+                sum += i;
+                i ++;
+        }
+        printf("sum = %d",sum);
+
+
+
+
+
+
+
+
+
+
+
+
+        /* int i,n,sum = 0; */
+        /* scanf("%d",&i); */
+        /* n = i; */
+        /* do */
+        /* { */
+        /*         sum += 1; */
+        /*         i ++; */
+        /* }while( i <= 10); */
+        /* printf("sum = %d",sum); */
+        return 0;
+}
