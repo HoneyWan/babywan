79ef11245d57f56c474e3a215312e4f9779b3a29
Tenderest <2087886585@qq.com>
2019-12-16 12:48:47 +0800

    Branch Delete

--- text/text.c
+++ text/text.c
@@ -5,7 +5,16 @@ int main(void)
         a = 1,b = 2,c = 3;
         printf("%d\n",b>c?b++:c++);
         printf("%d\n",b>c?b++:c++);
-        a<b?printf("%d\n",c):printf("%d",b);
+        /* a<b?printf("%d\n",c):printf("%d",b); */
+        if ( a< b )
+        {
+                printf("%d\n", c);
+        }
+        else
+        {
+                printf("%d", b);
+        }
+        
 
         return 0;
 }
--- text/text2.c
+++ text/text2.c
@@ -4,6 +4,6 @@ int main(void)
         int A,B,C;
         printf("请输入你的成绩:");
         scanf("%d",&A);
-        A > 90 ? printf("优秀") : A < 60 ? printf("未及格") : printf("及格");
+        A > 90 ? printf("a") : A < 60 ? printf("b") : printf("c");
         return 0;
 }
