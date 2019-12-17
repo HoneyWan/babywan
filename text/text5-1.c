c4d9e79c716991011fc20d8d3f3bf6dbb5856d81
SugerWan <1438621668@qq.com>
2019-12-17 15:40:04 +0800

    实训

---
+++ text/text5-1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#define N 10
+int main(void)
+{
+        int i,a[N],av;
+        for( i = 0 ; i < N ; i ++)
+        {
+                scanf("%d",a[i]);
+        }
+        for
+        return 0;
+}
