0ee38fd640b495643a064aca11a588a944ca3e93
Tenderest <2087886585@qq.com>
2019-12-18 15:24:42 +0800

    :pencil2: 修改了文件

--- 实训7/2.c
+++ 实训7/2.c
@@ -4,33 +4,54 @@
 #include <stdio.h>
 int main(void)
 {
-    int year, month, day, days = 0;
-    int a[12] = { 31, 28, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-    int b[12] = { 31, 29, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-    printf("请输入一个日期（格式示例2019 03 05）：");
-    scanf("%d%d%d", &year, &month, &day); // 输入日期
-    if ( month <= 2 )
-    {
-        days = 31 + day;
-    }
-    else
-    {
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) // 判断闰年
+        int year, month, day, days = 0;
+        int a[12] = { 31, 28, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        /* int b[12] = { 31, 29, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; */
+        printf("请输入一个日期（格式示例2019 03 05）：");
+        scanf("%d%d%d", &year, &month, &day); // 输入日期
+/* ( year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ) */
+        if ( year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ) // 先判断是不是润年
         {
-            // 闰年用数组b
-            for ( int i = 0; i < month; i ++ )
-            {
-                days = days + b[i];
-            }
-            days = days + day;
+                // 闰年
+                if ( month == 1 )
+                {
+                        days = days + day; // 1月直接等于输入天数day
+                }
+                else if ( month == 2 )
+                {
+                        days = a[0] + day; // 2月，加上1月和输入天数day
+                }
+                else
+                {
+                        // 超过2月
+                        for ( int i = 0; i < month - 1; i ++ )
+                        {
+                                days = days + a[i];
+                        }
+                        days = days + day + 1; // 闰年需要加1
+                }
         }
-        else
+        else// 平年
         {
-            // 平年
+                if ( month == 1 )
+                {
+                        days = days + day; // 1月直接等于输入天数day
+                }
+                else if ( month == 2 )
+                {
+                        days = a[0] + day; // 2月，加上1月和输入天数day
+                }
+                else
+                {
+                        // 超过2月
+                        for ( int i = 0; i < month - 1; i ++ )
+                        {
+                                days = days + a[i];
+                        }
+                        days = days + day; // 平年不用加1
+                }
         }
-    }
-    //printf("year = %d, month = %d, day = %d\n", year, month, day );
-    printf("Days = %d\n", days);
-    system("pause");
-    return 0;
+        //printf("year = %d, month = %d, day = %d\n", year, month, day );
+        printf("Days = %d\n", days);
+        return 0;
 }
