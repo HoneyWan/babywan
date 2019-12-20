7a4ec54e47a56774a1c78b52b4292f498089a7ae
Tenderest <2087886585@qq.com>
2019-12-20 09:42:19 +0800

    :tada: 第二个版本

---
+++ 实训7/2_1.c
@@ -0,0 +1,34 @@
+// 【程序2】 题目：输入某年某月某日，判断这一天是这一年的第几天？
+// 程序分析：以3月5日为例，应该先把前两个月的加起来，然后再加上5天即本年的第几天
+// 特殊情况，闰年且输入月份大于3时需考虑多加一天。
+#include <stdio.h>
+int main(void)
+{
+        int year, month, day, days = 0;
+        int a[12] = { 31, 28, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        printf("请输入一个日期（格式示例2019 03 05）：");
+        scanf("%d%d%d", &year, &month, &day); // 输入日期
+        if ( month <= 2 )
+        {
+                for ( int i = 0; i < month - 1; i ++ )
+                {
+                        days = days + a[i];
+                }
+                days = days + day;
+        }
+        else
+        {
+                // 超过2月
+                days = days + day;
+                for ( int i = 0; i < month - 1; i ++ )
+                {
+                        days = days + a[i];
+                }
+                if ( year % 4 == 0 && year % 100 != 0 || year % 400 == 0 )
+                {
+                        days = days + 1;
+                }
+        }
+        printf("Days = %d\n", days);
+        return 0;
+}
